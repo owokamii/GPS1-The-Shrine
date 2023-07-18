ebe7c2f3b01afe8e02d26aad7e923a77fefc01dd
owokamii <us3r424815@gmail.com>
2023-07-18 22:10:56 +0800

    22
    adjusted level 1 and some stuff

--- The Shrine Main/Assets/Scripts/Player/PlayerMovement.cs
+++ The Shrine Main/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,31 +5,32 @@ public class PlayerMovement : MonoBehaviour
     //references
     public CharacterController2D _controller;
     [SerializeField] private Rigidbody2D _rb;
-    public Animator _animator;
+    [SerializeField] private Animator _animator;
 
     //walk
     private float horizontalMove;
-    public float runSpeed = 40f;
+    private float runSpeed = 30f;
     bool jump = false;
     bool crouch = false;
 
     //climb
     private float verticalMove;
-    public float climbSpeed = 3f;
+    private float climbSpeed = 3f;
     bool _isLadder;
     bool _isClimbing;
 
     //push
-    public LayerMask boxMask;
-    public float _distance = 1f;
+    public LayerMask _crateMask;
+    private float _grabDistance = 0.6f;
     bool _isPushing;
 
+    //GameObjects
     AudioManager audioManager;
-    GameObject box;
+    GameObject crate;
 
     private void Awake()
     {
-        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+        //audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
     }
 
     void Update()
@@ -71,31 +72,34 @@ public class PlayerMovement : MonoBehaviour
             }
 
             Physics2D.queriesStartInColliders = false;
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, _distance, boxMask);
-            RaycastHit2D ground = Physics2D.Raycast(transform.position, Vector2.down * transform.localScale.x, _distance, boxMask);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, _grabDistance, _crateMask);
+            /*RaycastHit2D ground = Physics2D.Raycast(transform.position, Vector2.down * transform.localScale.x, _grabDistance, _crateMask);
             Debug.Log(ground.collider.gameObject.layer);
 
-            if(ground.collider.gameObject.layer == 9)
+            if (ground.collider.gameObject.layer == 9)
             {
-                _isClimbing = false;
-            }
+                _animator.SetBool("IsClimbing", false);
+            }*/
 
-            if (hit.collider != null && hit.collider.gameObject.tag == "Objects" && Input.GetButtonDown("Grab"))
+            if(hit.collider != null)
             {
-                box = hit.collider.gameObject;
-                box.GetComponent<FixedJoint2D>().enabled = true;
-                box.GetComponent<ObjectPhysics>().beingPushed = true; //not so optimized
-                box.GetComponent<FixedJoint2D>().connectedBody = this.GetComponent<Rigidbody2D>();
-                _isPushing = true;
-                _animator.SetBool("IsPushing", true);
-                audioManager.PlaySFX(audioManager.sfx[0]);
-            }
-            else if (Input.GetButtonUp("Grab"))
-            {
-                box.GetComponent<FixedJoint2D>().enabled = false;
-                box.GetComponent<ObjectPhysics>().beingPushed = false; //not so optimized
-                _isPushing = false;
-                _animator.SetBool("IsPushing", false);
+                if (hit.collider.gameObject.tag == "Objects" && Input.GetButtonDown("Grab")) //hit.collider != null
+                {
+                    crate = hit.collider.gameObject;
+                    crate.GetComponent<FixedJoint2D>().enabled = true;
+                    crate.GetComponent<ObjectPhysics>().beingPushed = true; //not so optimized
+                    crate.GetComponent<FixedJoint2D>().connectedBody = this.GetComponent<Rigidbody2D>();
+                    _isPushing = true;
+                    _animator.SetBool("IsPushing", true);
+                    audioManager.PlaySFX(audioManager.sfx[0]);
+                }
+                else if (Input.GetButtonUp("Grab"))
+                {
+                    crate.GetComponent<FixedJoint2D>().enabled = false;
+                    crate.GetComponent<ObjectPhysics>().beingPushed = false; //not so optimized
+                    _isPushing = false;
+                    _animator.SetBool("IsPushing", false);
+                }
             }
         }
     }
@@ -136,22 +140,13 @@ public class PlayerMovement : MonoBehaviour
         if (collision.CompareTag("Ladder"))
         {
             _isLadder = true;
-            if(Input.GetButtonDown("Vertical"))
-            {
-                _isClimbing = true;
-            }
         }
     }
 
     //climbing ladder
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.CompareTag("Ladder"))
-        {
-            //probably use raycast to detect if theres a ground, if there is: animation false, if there isn't: animation true
-            _isClimbing = true;
-            _animator.SetBool("IsClimbing", true);
-        }
+        //probably use raycast to detect if theres a ground, if there is: animation false, if there isn't: animation true
     }
 
     //exit ladder
@@ -168,6 +163,6 @@ public class PlayerMovement : MonoBehaviour
     void OnDrawGizmos()
     {
         Gizmos.color = Color.cyan;
-        Gizmos.DrawLine(transform.position, (Vector2)transform.position + Vector2.right * transform.localScale.x * _distance);
+        Gizmos.DrawLine(transform.position, (Vector2)transform.position + Vector2.right * transform.localScale.x * _grabDistance);
     }
 }
