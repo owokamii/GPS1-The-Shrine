bf69e03fde30434f250ccd1a8f5a2b1fc4e47e24
owokamii <us3r424815@gmail.com>
2023-07-20 14:02:06 +0800

    25
    level 1 adjustments

--- The Shrine Main/Assets/Scripts/Player/PlayerHealth.cs
+++ The Shrine Main/Assets/Scripts/Player/PlayerHealth.cs
@@ -58,7 +58,7 @@ public class PlayerHealth : MonoBehaviour
 
     void Dehydration()
     {
-        _currentThirst -= 1.5f * Time.deltaTime;
+        _currentThirst -= 2 * Time.deltaTime;
         _thirstBar.SetThirst(_currentThirst);
     }
 
