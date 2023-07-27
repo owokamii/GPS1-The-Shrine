bf6b3cfdf2d76b3c91f7ce02144866fe3e949c38
Yejinhand69 <0134674@student.uow.edu.my>
2023-07-28 02:22:13 +0800

    enemy still not respawning correctly

--- The Shrine Main/Assets/Scripts/Enemy/hide.cs
+++ The Shrine Main/Assets/Scripts/Enemy/hide.cs
@@ -6,15 +6,19 @@ public class hide : MonoBehaviour
 {
     public GameObject bird;
     public float moveSpeed;
+    private Animator animator;
+    public Transform spawnPoint;
     // Start is called before the first frame update
     void Start()
     {
+
         bird.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        animator = GetComponent<Animator>();
         GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, 0f);
     }
 
@@ -24,8 +28,19 @@ public class hide : MonoBehaviour
         {
             // Code to handle the collision with the detector
             Debug.Log("Player kill");
-            Destroy(collision.gameObject);
+            //Destroy(collision.gameObject);
             Destroy(gameObject);
+            Instantiate(bird, spawnPoint.position, Quaternion.identity);
+            
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("StopDetector"))
+        {
+            moveSpeed = 0f;
+            animator.SetTrigger("StopDetector");
         }
     }
 
--- The Shrine Main/Assets/Scripts/Enemy/unhide.cs
+++ The Shrine Main/Assets/Scripts/Enemy/unhide.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class unhide : MonoBehaviour
 {
     public GameObject bird;
+    public Transform spawnPoint;
     // Start is called before the first frame update
     //public void OnCollisionEnter2D(Collision2D collision)
     //{
@@ -22,6 +23,7 @@ public class unhide : MonoBehaviour
         {
             // Code to handle the collision with the detector
             Debug.Log("Player collided with detector");
+
             bird.SetActive(true);
         }
     }
